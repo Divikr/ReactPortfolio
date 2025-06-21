18742b7ba9be9a5b68016c1ce445def013eb543e
Divikr <divith2526@gmail.com>
2025-06-21 13:35:13 +0530

    second

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,70 +1,47 @@
-import React,{useState, useRef} from 'react'
-import emailjs from "@emailjs/browser";
+import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
-import './Contact.css'
+import './Contact.css';
 
 const Contact = () => {
-    const form = useRef();
-    const [done, setDone] = useState(false)
-    const [notDone, setNotDone] = useState(false)
-    const [formData, setFormData] = useState({});
+  return (
+    <Container style={{ paddingTop: '50px' }}>
+      <Row>
+        <Col md={6} className="c-left">
+          <h1>Get in Touch</h1>
+          <h1 >Contact me</h1>
+        </Col>
 
-    const handleChange = (e) => {
-        setFormData({...formData, [e.target.name] : e.target.value})
-        setDone(false)
-        setNotDone(false)
-    }
+        <Col md={6} className="c-right">
+          <form
+            action="https://formspree.io/f/mnqkyxyz" 
+            method="POST"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+              className="user"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+              className="user"
+            />
+            <textarea
+              name="message"
+              placeholder="Message"
+              required
+              className="user"
+            />
+            <button type="submit" className="button">Send</button>
+          </form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-    const sendEmail = (e) => {
-    e.preventDefault();
-    
-    if(!formData.from_name || !formData.reply_to ||!formData.message){
-      setNotDone(true)
-    } else {
-      
-      //  Please use your own credentials from emailjs or i will recive your email
-      
-    emailjs
-      .sendForm(
-        "service_niilndo",
-        "template_6z5idye",
-        form.current,
-        "VOBt6Akm1LhI5CZG-"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    }
-    };
-    
-
-    return(
-        <Container style={{paddingTop: '50px'}} >
-            <Row >
-            <Col md={6} className="c-left" >
-            <h1 >Get in Touch</h1>
-            <h1 className="yellow">Contact me</h1>
-            </Col>
-            <Col md={6} className="c-right">
-                <form ref={form} onSubmit={sendEmail}>
-                <input type="text" name="from_name" className="user"  placeholder="Name" onChange={handleChange}/>
-                <input type="email" name="reply_to" className="user" placeholder="Email" onChange={handleChange} />
-                <textarea name="message" className="user" placeholder="Message" onChange={handleChange} />
-                <span className='not-done' >{notDone && "Please, fill all the input field"}</span>
-                <Button type="submit" className="button" disabled={done}>Send</Button>
-                <span className='done'>{done && "Thanks for contacting me and be sure i have recieved your mail. If you are testing this functionality then i am confirming this thing working perfectly fine. If you have any serious query then i will reply. Also if you need me, you can conatct me on Linkedin."}</span>
-                </form>
-            </Col>
-            </Row>
-        </Container>
-    )
-}
-
-export default Contact
+export default Contact;
